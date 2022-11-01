b0257eb3903108184745b227bfdbba0dcecf37bc
Dylan Cope <dylanr.cope@gmail.com>
2022-11-01 15:52:26 +0000

    changed wall drawing

--- gym_maze/envs/maze_view_2d.py
+++ gym_maze/envs/maze_view_2d.py
@@ -145,17 +145,7 @@ class MazeView2D:
         
         line_colour = (0, 0, 0, 255)
 
-        # drawing the horizontal lines
-        for y in range(self.maze.MAZE_H + 1):
-            pygame.draw.line(self.maze_layer, line_colour, (0, y * self.CELL_H),
-                             (self.SCREEN_W, y * self.CELL_H), width=self.line_width)
-
-        # drawing the vertical lines
-        for x in range(self.maze.MAZE_W + 1):
-            pygame.draw.line(self.maze_layer, line_colour, (x * self.CELL_W, 0),
-                             (x * self.CELL_W, self.SCREEN_H), width=self.line_width)
-
-        # breaking the walls
+        # draw openings
         for x in range(len(self.maze.maze_cells)):
             for y in range (len(self.maze.maze_cells[x])):
                 # check the which walls are open in each cell
@@ -165,6 +155,17 @@ class MazeView2D:
                     if open:
                         dirs += dir
                 self.__cover_walls(x, y, dirs)
+        
+        # draw walls
+        for x in range(len(self.maze.maze_cells)):
+            for y in range (len(self.maze.maze_cells[x])):
+                # check the which walls are open in each cell
+                walls_status = self.maze.get_walls_status(self.maze.maze_cells[x, y])
+                dirs = ""
+                for dir, open in walls_status.items():
+                    if not open:
+                        dirs += dir
+                self.__cover_walls(x, y, dirs, colour=line_colour)
 
     def __cover_walls(self, x, y, dirs, colour=(0, 0, 255, 15)):
 
